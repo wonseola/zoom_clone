dc860efaa715d0d151b9ec991377c6155cf37bc1
WonSeola <39373661+wonseola@users.noreply.github.com>
2024-04-25 23:24:58 +0900

    socket.io

--- src/server.js
+++ src/server.js
@@ -1,14 +1,28 @@
 import http from "http";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
 import express from "express";
+import { instrument } from "@socket.io/admin-ui"
+
+
 const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true
+    }
+});
+
+instrument(wsServer, {
+    auth: false,
+    mode: "development",
+});
 
 function publicRooms() {
     const { sockets: { adapter: { sids, rooms }, }, } = wsServer;
